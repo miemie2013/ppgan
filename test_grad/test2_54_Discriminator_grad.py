311f63d8315a240e2894a0e3177a33f3ba2590ec
miemie2013 <53960695+miemie2013@users.noreply.github.com>
2022-03-15 12:42:59 +0800

    f

--- test_grad/test2_54_Discriminator_grad.py
+++ test_grad/test2_54_Discriminator_grad.py
@@ -1,7 +1,7 @@
-
 import torch
 import numpy as np
 from training.networks import Discriminator
+from training.augment import AugmentPipe
 
 
 c_dim = 0
@@ -39,6 +39,45 @@ model.train()
 optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9)
 torch.save(model.state_dict(), "54.pth")
 
+
+# 所有，除了noise = 0
+xflip = 1
+rotate90 = 1
+xint = 1
+xint_max = 0.125
+scale = 1
+rotate = 1
+aniso = 1
+xfrac = 1
+scale_std = 0.2
+rotate_max = 1
+aniso_std = 0.2
+xfrac_std = 0.125
+brightness = 1
+contrast = 1
+lumaflip = 1
+hue = 1
+saturation = 1
+brightness_std = 0.2
+contrast_std = 0.5
+hue_max = 1
+saturation_std = 1
+imgfilter = 1
+imgfilter_bands = [1, 1, 1, 1]
+imgfilter_std = 1
+noise = 0
+cutout = 1
+noise_std = 0.1
+cutout_size = 0.5
+
+augment_pipe = AugmentPipe(xflip, rotate90, xint, xint_max,
+        scale, rotate, aniso, xfrac, scale_std, rotate_max, aniso_std, xfrac_std,
+        brightness, contrast, lumaflip, hue, saturation, brightness_std, contrast_std, hue_max, saturation_std,
+        imgfilter, imgfilter_bands, imgfilter_std,
+        noise, cutout, noise_std, cutout_size)
+
+
+debug_percentile = 0.7
 dic = {}
 for batch_idx in range(8):
     optimizer.zero_grad(set_to_none=True)
@@ -46,7 +85,8 @@ for batch_idx in range(8):
     x = torch.randn(x_shape)
     x.requires_grad_(True)
 
-    y = model(x, None)
+    aug_x = augment_pipe(x, debug_percentile)
+    y = model(aug_x, None)
     dy_dx = torch.autograd.grad(outputs=[y.sum()], inputs=[x], create_graph=True, only_inputs=True)[0]
 
     dic['batch_%.3d.dy_dx'%batch_idx] = dy_dx.cpu().detach().numpy()
--- test_grad/test2_54_Discriminator_grad_paddle.py
+++ test_grad/test2_54_Discriminator_grad_paddle.py
@@ -1,7 +1,7 @@
-
 import paddle
 import numpy as np
 from ppgan.models.discriminators.discriminator_styleganv2ada import StyleGANv2ADA_Discriminator
+from ppgan.models.generators.generator_styleganv2ada import StyleGANv2ADA_AugmentPipe
 
 
 c_dim = 0
@@ -39,6 +39,45 @@ optimizer = paddle.optimizer.Momentum(parameters=model.parameters(), learning_ra
 model.set_state_dict(paddle.load("54.pdparams"))
 
 
+# 所有，除了noise = 0
+xflip = 1
+rotate90 = 1
+xint = 1
+xint_max = 0.125
+scale = 1
+rotate = 1
+aniso = 1
+xfrac = 1
+scale_std = 0.2
+rotate_max = 1
+aniso_std = 0.2
+xfrac_std = 0.125
+brightness = 1
+contrast = 1
+lumaflip = 1
+hue = 1
+saturation = 1
+brightness_std = 0.2
+contrast_std = 0.5
+hue_max = 1
+saturation_std = 1
+imgfilter = 1
+imgfilter_bands = [1, 1, 1, 1]
+imgfilter_std = 1
+noise = 0
+cutout = 1
+noise_std = 0.1
+cutout_size = 0.5
+
+
+augment_pipe = StyleGANv2ADA_AugmentPipe(xflip, rotate90, xint, xint_max,
+        scale, rotate, aniso, xfrac, scale_std, rotate_max, aniso_std, xfrac_std,
+        brightness, contrast, lumaflip, hue, saturation, brightness_std, contrast_std, hue_max, saturation_std,
+        imgfilter, imgfilter_bands, imgfilter_std,
+        noise, cutout, noise_std, cutout_size)
+
+
+debug_percentile = 0.7
 dic2 = np.load('54.npz')
 for batch_idx in range(8):
     print('======================== batch_%.3d ========================'%batch_idx)
@@ -49,11 +88,13 @@ for batch_idx in range(8):
 
     x = paddle.to_tensor(x)
     x.stop_gradient = False
-    y = model(x, None)
+    aug_x = augment_pipe(x, debug_percentile)
+    y = model(aug_x, None)
 
     # dy_dx = paddle.grad(outputs=[y.sum()], inputs=[x], create_graph=True)[0]
     dysum_dy = paddle.ones(y.shape, dtype=paddle.float32)
-    dy_dx = model.grad_layer(dysum_dy)
+    dy_daug_x = model.grad_layer(dysum_dy)
+    dy_dx = augment_pipe.grad_layer(dy_daug_x)
 
 
     y_paddle = y.numpy()
