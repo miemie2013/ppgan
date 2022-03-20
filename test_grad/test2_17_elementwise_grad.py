b36158f8a435f0b7092175a94a05da8a5f13d115
miemie2013 <53960695+miemie2013@users.noreply.github.com>
2022-03-21 00:02:03 +0800

    f

--- test_grad/test2_17_elementwise_grad.py
+++ test_grad/test2_17_elementwise_grad.py
@@ -12,13 +12,11 @@ import torch.nn.functional as F
 
 dic = {}
 for batch_idx in range(8):
-    styles = torch.randn([2, 512])
+    styles = torch.randn([2, 512, 512])
     styles.requires_grad_(True)
-    weight = torch.randn([512, 512])
     batch_size = 2
 
-    w = weight.unsqueeze(0) # [1, out_C, in_C]
-    w = w * styles.reshape(batch_size, 1, -1) # [N, out_C, in_C]
+    w = styles * 11
     dcoefs = w.sum(dim=[2,])  # [N, out_C]
     out = w * dcoefs.reshape((batch_size, -1, 1))  # [N, out_C, in_C]
     loss = torch.square(out)
@@ -27,7 +25,6 @@ for batch_idx in range(8):
 
     dic['batch_%.3d.dloss_dstyles'%batch_idx] = dloss_dstyles.cpu().detach().numpy()
     dic['batch_%.3d.out'%batch_idx] = out.cpu().detach().numpy()
-    dic['batch_%.3d.weight'%batch_idx] = weight.cpu().detach().numpy()
     dic['batch_%.3d.styles'%batch_idx] = styles.cpu().detach().numpy()
 np.savez('17_grad', **dic)
 print()
--- test_grad/test2_17_elementwise_grad_paddle.py
+++ test_grad/test2_17_elementwise_grad_paddle.py
@@ -16,19 +16,24 @@ for batch_idx in range(8):
     styles = dic2['batch_%.3d.styles'%batch_idx]
     styles = paddle.to_tensor(styles)
     styles.stop_gradient = False
-    weight = dic2['batch_%.3d.weight'%batch_idx]
-    weight = paddle.to_tensor(weight)
-    weight.stop_gradient = False
 
     batch_size = 2
 
-    w = weight.unsqueeze(0) # [1, out_C, in_C]
-    w1 = w * styles.reshape((batch_size, 1, -1)) # [N, out_C, in_C]
-    dcoefs = w1.sum(axis=[2,])  # [N, out_C]
-    out = w1 * dcoefs.reshape((batch_size, -1, 1))  # [N, out_C, in_C]
+
+    w1 = styles * 11
+    # dcoefs = w1.sum(axis=[2,])  # [N, out_C]
+    # out = w1 * dcoefs.reshape((batch_size, -1, 1))  # [N, out_C, in_C]
+    out = w1 * paddle.sum(w1, axis=2, keepdim=True)
     loss = paddle.square(out)
 
     dloss_dstyles = paddle.grad(outputs=[loss.sum()], inputs=[styles], create_graph=True)[0]
+
+    # paddle.grad()，dloss_dw1计算错误，把paddle.sum(w1, axis=2, keepdim=True)看成了常数，
+    # 实际上paddle.sum(w1, axis=2, keepdim=True)里也有w1的表达式。
+    dloss_dloss = paddle.ones(loss.shape, dtype=paddle.float32)
+    dloss_dout = dloss_dloss * 2 * out
+    dloss_dw1 = dloss_dout * paddle.sum(w1, axis=2, keepdim=True)
+    dloss_dstyles222 = dloss_dw1 * 11
 
 
     out_paddle = out.numpy()
@@ -37,6 +42,10 @@ for batch_idx in range(8):
 
     dloss_dstyles_paddle = dloss_dstyles.numpy()
     ddd = np.mean((dloss_dstyles_pytorch - dloss_dstyles_paddle) ** 2)
+    print('ddd=%.6f' % ddd)
+
+    dloss_dstyles222_paddle = dloss_dstyles222.numpy()
+    ddd = np.mean((dloss_dstyles222_paddle - dloss_dstyles_paddle) ** 2)
     print('ddd=%.6f' % ddd)
     print()
 print()
